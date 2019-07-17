c7e9d1ad93f831e2016bf8cdb70c1f6093ecd40e
Tomasz Szymanski <tomasz.szymanski@currenda.pl>
2019-07-17 08:56:14 +0200

    TicTacToe

--- Project 1 - TicTacToe/src/main/java/com/udacity/Game.java
+++ Project 1 - TicTacToe/src/main/java/com/udacity/Game.java
@@ -1,6 +1,8 @@
 package com.udacity;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by udacity 2016
@@ -10,13 +12,12 @@ public class Game {
 
     private char turn; // who's turn is it, 'x' or 'o' ? x always starts
     private boolean twoPlayer; // true if this is a 2 player game, false if AI playing
-    private char [][] grid; // a 2D array of chars representing the game grid
+    private char[][] grid; // a 2D array of chars representing the game grid
     private int freeSpots; // counts the number of empty spots remaining on the board (starts from 9  and counts down)
     private static GameUI gui;
 
     /**
      * Create a new single player game
-     *
      */
     public Game() {
         newGame(false);
@@ -24,17 +25,18 @@ public class Game {
 
     /**
      * Create a new game by clearing the 2D grid and restarting the freeSpots counter and setting the turn to x
+     *
      * @param twoPlayer: true if this is a 2 player game, false if playing against the computer
      */
-    public void newGame(boolean twoPlayer){
+    public void newGame(boolean twoPlayer) {
         //sets a game to one or two player
         this.twoPlayer = twoPlayer;
 
         // initialize all chars in 3x3 game grid to '-'
         grid = new char[3][3];
         //fill all empty slots with -
-        for (int i=0; i<3; i++){
-            for (int j=0; j<3; j++){
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
                 grid[i][j] = '-';
             }
         }
@@ -47,16 +49,17 @@ public class Game {
 
     /**
      * Gets the char value at that particular position in the grid array
+     *
      * @param i the x index of the 2D array grid
      * @param j the y index of the 2D array grid
      * @return the char value at the position (i,j):
-     *          'x' if x has played here
-     *          'o' if o has played here
-     *          '-' if no one has played here
-     *          '!' if i or j is out of bounds
+     * 'x' if x has played here
+     * 'o' if o has played here
+     * '-' if no one has played here
+     * '!' if i or j is out of bounds
      */
-    public char gridAt(int i, int j){
-        if(i>=3||j>=3||i<0||j<0)
+    public char gridAt(int i, int j) {
+        if (i >= 3 || j >= 3 || i < 0 || j < 0)
             return '!';
         return grid[i][j];
     }
@@ -64,16 +67,17 @@ public class Game {
     /**
      * Places current player's char at position (i,j)
      * Uses the variable turn to decide what char to use
+     *
      * @param i the x index of the 2D array grid
      * @param j the y index of the 2D array grid
      * @return boolean: true if play was successful, false if invalid play
      */
-    public boolean playAt(int i, int j){
+    public boolean playAt(int i, int j) {
         //check for index boundries
-        if(i>=3||j>=3||i<0||j<0)
+        if (i >= 3 || j >= 3 || i < 0 || j < 0)
             return false;
         //check if this position is available
-        if(grid[i][j] != '-'){
+        if (grid[i][j] != '-') {
             return false; //bail out if not available
         }
         //update grid with new play based on who's turn it is
@@ -86,14 +90,16 @@ public class Game {
 
     /**
      * Override
+     *
      * @return string format for 2D array values
      */
-    public String toString(){
+    public String toString() {
         return Arrays.deepToString(this.grid);
     }
 
     /**
      * Performs the winner chack and displayes a message if game is over
+     *
      * @return true if game is over to start a new game
      */
     public boolean doChecks() {
@@ -110,29 +116,27 @@ public class Game {
     /**
      * Allows computer to play in a single player game or switch turns for 2 player game
      */
-    public void nextTurn(){
+    public void nextTurn() {
         //check if single player game, then let computer play turn
-        if(!twoPlayer){
-            if(freeSpots == 0){
-                return ; //bail out if no more free spots
+        if (!twoPlayer) {
+            if (freeSpots == 0) {
+                return; //bail out if no more free spots
             }
             int ai_i, ai_j;
             do {
                 //randomly pick a position (ai_i,ai_j)
                 ai_i = (int) (Math.random() * 3);
                 ai_j = (int) (Math.random() * 3);
-            }while(grid[ai_i][ai_j] != '-'); //keep trying if this spot was taken
+            } while (grid[ai_i][ai_j] != '-'); //keep trying if this spot was taken
             //update grid with new play, computer is always o
             grid[ai_i][ai_j] = 'o';
             //update free spots
             freeSpots--;
-        }
-        else{
+        } else {
             //change turns
-            if(turn == 'x'){
+            if (turn == 'x') {
                 turn = 'o';
-            }
-            else{
+            } else {
                 turn = 'x';
             }
         }
@@ -145,20 +149,21 @@ public class Game {
      * If game hasn't ended the return string has to be "None",
      * If the game ends as tie, the return string has to be "Tie",
      * If the game ends because there's a winner, it should return "X wins" or "O wins" accordingly
+     *
      * @param grid 2D array of characters representing the game board
      * @return String indicating the outcome of the game: "X wins" or "O wins" or "Tie" or "None"
      */
-    public String checkGameWinner(char [][]grid){
-        String result = "None";
-        //Student code goes here ...
-        return result;
+    public String checkGameWinner(char[][] grid) {
+        Result result = new Result(grid);
+        return result.getResult();
     }
 
     /**
      * Main function
+     *
      * @param args command line arguments
      */
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Game game = new Game();
         gui = new GameUI(game);
     }
---
+++ Project 1 - TicTacToe/src/main/java/com/udacity/Result.java
@@ -0,0 +1,118 @@
+package com.udacity;
+
+import java.util.List;
+
+class Result {
+
+    private char[][] grid;
+
+    Result(char[][] grid) {
+        this.grid = grid;
+    }
+
+    String getResult() {
+
+        String result;
+
+        result = checkRows();
+        if (!result.isEmpty())
+            return result;
+
+        result = checkCollumns();
+        if (!result.isEmpty())
+            return result;
+
+        result = checkLeftDiagonal();
+        if (!result.isEmpty())
+            return result;
+
+        result = checkRightDiagonal();
+        if (!result.isEmpty())
+            return result;
+
+        result = checkNone();
+        if (!result.isEmpty())
+            return result;
+
+        return "Tie";
+    }
+
+    public interface O
+    {
+        String xxx();
+    }
+
+    private String checkRows() {
+        for (int row = 0; row <= 2; row++) {
+            char ch = grid[row][0];
+            boolean won = true;
+            for (int col = 0; col <= 2; col++) {
+                if (ch != grid[row][col] || ch == '-') {
+                    won = false;
+                    break;
+                }
+            }
+            if (won)
+                return String.format("%c wins", ch);
+        }
+
+        return "";
+    }
+
+    private String checkCollumns() {
+
+        for (int col = 0; col <= 2; col++) {
+            char ch = grid[0][col];
+            boolean won = true;
+            for (int row = 0; row <= 2; row++) {
+                if (ch != grid[row][col] || ch == '-') {
+                    won = false;
+                    break;
+                }
+            }
+            if (won)
+                return String.format("%c wins", ch);
+        }
+        return "";
+    }
+
+    private String checkLeftDiagonal() {
+        boolean won = true;
+        char ch = grid[0][0];
+        for (int row = 0, col = 0; row <= 2; row++, col++) {
+            if (ch != grid[row][col] || ch == '-') {
+                won = false;
+                break;
+            }
+        }
+        return won ? String.format("%c wins", ch) : "";
+    }
+
+    private String checkRightDiagonal() {
+        char ch = grid[0][2];
+        boolean won = true;
+        for (int row = 0, col = 2; row <= 2; row++, col--) {
+            if (ch != grid[row][col] || ch == '-') {
+                won = false;
+                break;
+            }
+        }
+        return won ? String.format("%c wins", ch) : "";
+    }
+
+    private String checkNone() {
+        boolean tie = true;
+        for (int row = 0; row <= 2; row++) {
+            if (!tie)
+                break;
+
+            for (int col = 0; col <= 2; col++) {
+                if ('-' == grid[row][col]) {
+                    tie = false;
+                    break;
+                }
+            }
+        }
+        return !tie ? "None" : "";
+    }
+}
